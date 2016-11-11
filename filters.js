6dfad5fd6200f889790280d6b6d2e7ade2f50258
Saturnidies <saturnidies@mail.ru>
2016-11-11 11:44:01 +0300

    Filters, IT WORKS :D

---
+++ filters.js
@@ -0,0 +1,42 @@
+function normal()
+{
+    document.getElementById('foo').setAttribute("class", "example1");
+}
+
+function sepia()
+{
+    document.getElementById('foo').setAttribute("class", "example2");
+}
+function grey()
+{
+    document.getElementById('foo').setAttribute("class", "example3");
+}
+function contrast()
+{
+    document.getElementById('foo').setAttribute("class", "example4");
+}
+
+function saturation()
+{
+    document.getElementById('foo').setAttribute("class", "example5");
+}
+function inversion()
+{
+    document.getElementById('foo').setAttribute("class", "example6");
+}
+function degree1()
+{
+    document.getElementById('foo').setAttribute("class", "example7");
+}
+function degree2()
+{
+    document.getElementById('foo').setAttribute("class", "example8");
+}
+function degree3()
+{
+    document.getElementById('foo').setAttribute("class", "example9");
+}
+function legion()
+{
+    document.getElementById('foo').setAttribute("class", "example10");
+}
